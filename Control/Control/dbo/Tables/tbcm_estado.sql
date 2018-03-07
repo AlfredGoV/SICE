260d86d58912bf552af20f0bc679700cb7c0603d
Alfredo Pedraza <pedraza.alfredo@gmail.com>
2018-03-07 15:16:41 -0600

    Se Crea Esquema de la BD para Control

---
+++ Control/Control/dbo/Tables/tbcm_estado.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tbcm_estado] (
+    [idEstado] VARCHAR (4)  NOT NULL,
+    [NoEstado] SMALLINT     NULL,
+    [Estado]   VARCHAR (26) NULL,
+    CONSTRAINT [PK_tbcm_estado] PRIMARY KEY CLUSTERED ([idEstado] ASC)
+);
+
---
+++ Control/Control/dbo/Tables/tc_Empresa.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tc_Empresa] (
+    [idEmpresa] TINYINT      NOT NULL,
+    [empresa]   VARCHAR (50) NULL,
+    [rfc]       VARCHAR (13) NULL,
+    CONSTRAINT [PK_tc_catEmpresa] PRIMARY KEY NONCLUSTERED ([idEmpresa] ASC) WITH (FILLFACTOR = 90)
+);
+
